1647858cffc95a5cadbcc3a7148d393e39725bd5
robles153 <marcoskim153@gmail.com>
2022-10-30 12:42:55 -0300

    Adicionar arquivos de projeto.

---
+++ RestWithASPNET/Business/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,53 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IPersonRepository _repository;
+
+        public PersonBusinessImplementation(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            return _repository.FindAll();
+        }
+
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+
+        public Person Create(Person person)
+        {
+          
+            return _repository.Create(person);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        
+        }
+            
+    }
+}
